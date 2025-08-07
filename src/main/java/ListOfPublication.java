c33959cd0066887747ca7e7081794014c173b33d
RahJess <jessicakantotiana@gmail.com>
2025-08-07 09:47:44 +0200

    add: method to search publication by a word giving in parameters

--- src/main/java/ListOfPublication.java
+++ src/main/java/ListOfPublication.java
@@ -15,19 +15,25 @@ public class ListOfPublication {
 
     public static boolean searchPublicationByWord(String word){
         for (Publication publication : publicationsList) {
-            if (!Publication.getContent().toLowerCase().contains(word)){
-                System.out.println("Aucune publication ne correspond au mot : " + word);
-            } else {
+            if (Publication.getContent().toLowerCase().contains(word.toLowerCase())) {
                 System.out.println(publication);
+            } else {
+                System.out.println("No publication match with : " + word);
             }
         }
         return false;
     }
 
     public static void main(String[] args) {
-        List<Publication> list = new ArrayList<Publication>();
-        Publication post2 = new Publication("Hello everyone !", LocalDate.now());
-        list.add(post2);
-        System.out.println(searchPublicationByWord("Hello"));
+        ListOfPublication listOfPublication = new ListOfPublication(publicationsList);
+        Publication post = new Publication("Hello everyone", LocalDate.of(2025, 12, 12));
+        Publication post2 = new Publication("Let's code today !", LocalDate.of(2025, 11, 10));
+
+        publicationsList = new ArrayList<>();
+        publicationsList.add(post);
+        publicationsList.add(post2);
+
+        System.out.println(listOfPublication.getPublicationsList());
+        listOfPublication.searchPublicationByWord("Let's code today !");
     }
 }
